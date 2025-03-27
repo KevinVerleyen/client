01a998b5c2b0121a5a4dd46c9ee44540d9bf3147
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-03-27 15:05:50 +0100

    post & comment

---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import PostListItem from "./PostListItem";
+
+const PostList = () => {
+  return (
+    <div className="">
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+    </div>
+  );
+};
+
+export default PostList;
