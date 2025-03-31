8393dd09dc40085e5ebf5c49479b0484dc1b820d
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-03-31 09:52:24 +0200

    bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         </div>
         {/* MOBILE LINK LIST */}
         <div
-          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-in-out ${
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e5e5e5] transition-all ease-in-out ${
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
